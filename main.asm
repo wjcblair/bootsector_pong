4a9b3f23d4375e752e912564297357d4de77abb5
William Blair <w.blair@lancaster.ac.uk>
2023-12-06 13:47:33 +0000

    Add middle line with delay

--- main.asm
+++ main.asm
@@ -1,6 +1,6 @@
 use16
 
-org 07C00h
+org 7C00h
 
 ;; Set up video mode
 mov ah, 00h		; set BIOS video mode 
@@ -21,12 +21,29 @@ game_loop:
 	;; stosw is equivalent to:
 	;;						mov [es:di], ax
 	;;						inc di
+	
+	;; Draw middle separating line
+	mov ah, 0F0h
+	mov di, 78		; Start at middle of 80 char row
+	mov cx, 13		; 'Dashed' line - only draw every other row
+	.draw_middle_line:
+		stosw					; Store the contents of the AX register in DI, this autoincrements by sizeof str being stored
+		add di, 320-2			; Move to next position (two rows down)
+		loop .draw_middle_line	; Loop according to CX and decrement	
 
 ;; Draw stuff to the screen
 
 ;; Player input
 
 ;; CPU input
+
+	;; Delay timer (because CPU draws too fast)
+	mov bx, [046Ch] ; (from BIOS Data Area (BDA)) Move # of IRQ0 timer ticks since boot into bx
+	inc bx
+	inc bx	; Could do add instead but two incs saves space
+	.delay:	; Compare the number current number of ticks with bx, if difference < 2, delay again
+		cmp [046Ch], bx
+		jl .delay
 
 jmp game_loop
 
